22528a1a6b8609bb9d237774eee5367e11ac7375
Alexandr <alsam3637@gmail.com>
2020-12-23 07:23:51 -0500

    Create Request Router

---
+++ core/Router.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\core;
+/**
+ * Class Router
+ * @package app\core
+ */
+class Router
+{
+    public Request $request;
+    protected array $routes = [];
+
+    /**
+     * Router constructor.
+     * @param \app\core\Request $request
+     */
+    public function __construct(\app\core\Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if ($callback === false) {
+            echo 'Not found';
+            exit;
+        }
+        echo call_user_func($callback);
+        /*echo '<pre>';
+        var_dump($callback);
+        echo '</pre>';
+        exit;*/
+    }
+}
