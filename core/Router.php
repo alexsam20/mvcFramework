8bc23c1918d29ea232b8c9655f0db89107e147fc
Alexandr <alsam3637@gmail.com>
2020-12-23 09:00:30 -0500

    create gitignore MVC folder and connections bootstrap

--- core/Router.php
+++ core/Router.php
@@ -30,13 +30,20 @@ class Router
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
         if ($callback === false) {
-            echo 'Not found';
-            exit;
+            return 'Not found';
         }
-        echo call_user_func($callback);
+        if (is_string($callback)) {
+            return $this->renderView($callback);
+        }
+        return call_user_func($callback);
         /*echo '<pre>';
         var_dump($callback);
         echo '</pre>';
         exit;*/
+    }
+
+    public function renderView($view)
+    {
+        include_once __DIR__ . "/../views/$view.php";
     }
 }
