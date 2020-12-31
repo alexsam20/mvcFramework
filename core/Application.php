ef1aba42603a9c38a70951c8782f495ff9e05a97
Alexandr <alsam3637@gmail.com>
2020-12-30 23:20:19 -0500

    Handling submitted data

--- core/Application.php
+++ core/Application.php
@@ -14,13 +14,15 @@ class Application
     public Router $router;
     public Request $request;
     public Response $response;
+    public Session $session;
 
     public function __construct($rootPath, array $config)
     {
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
-        $this->request = new Request;
-        $this->response = new Response;
+        $this->request = new Request();
+        $this->response = new Response();
+        $this->session = new Session();
         $this->router = new Router($this->request, $this->response);
         $this->db = new Database($config['db']);
     }
